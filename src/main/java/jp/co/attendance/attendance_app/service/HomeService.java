f2ed2092d9d7ebfc819969cd8121c1515decf3d2
khiromichi <kameda.hiromichi0518@gmail.com>
2026-04-16 16:48:23 +0900

    SCR002: HomeServiceを追加

---
+++ src/main/java/jp/co/attendance/attendance_app/service/HomeService.java
@@ -0,0 +1,11 @@
+package jp.co.attendance.attendance_app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public void initializeHome() {
+        // PL002 ホーム画面初期表示の業務処理は今後ここに実装する
+    }
+}
